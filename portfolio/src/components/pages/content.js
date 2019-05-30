e93abe8cb23263159cf48e9624e061d78b55d322
FrankDaTankB <36753033+FrankDaTankB@users.noreply.github.com>
2019-05-29 22:45:44 -0400

    intital commit

--- portfolio/src/components/pages/content.js
+++ portfolio/src/components/pages/content.js
@@ -82,7 +82,7 @@ class Content extends Component {
               <li>Cleaning tables after customers have left</li>
               <li>Maintaining a clean and fully stocked floor</li>
               <li>Serving customers if requested</li>
-            </ul>
+            </ul>    
             </p>
           </div>
           <div className="resume-date text-md-right">
@@ -205,7 +205,9 @@ class Content extends Component {
     <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
       <div className="w-100">
         <h2 className="mb-5">Interests</h2>
-        <p>In production</p>
+        <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+        <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end
+          web development world.</p>
       </div>
     </section>
 
