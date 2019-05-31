7aaf7c5eb65f5a79da97afc643abb48a9964c289
FrankDaTankB <36753033+FrankDaTankB@users.noreply.github.com>
2019-05-31 16:33:39 -0400

    update v1.0

--- portfolio/src/components/pages/content.js
+++ portfolio/src/components/pages/content.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactTooltip from 'react-tooltip'
 
 //components
 
@@ -7,7 +8,6 @@ class Content extends Component {
   render() {
     return (
   <div className="container-fluid p-0" id="page-top">
-  <div className="bg-img">
     <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
       <div className="w-100">
         <h1 className="mb-0">Frank
@@ -88,8 +88,11 @@ class Content extends Component {
           <div className="resume-content">
             <h3 className="mb-0">Takeout Associate/Delivery Driver</h3>
             <div className="subheading mb-3">Marietta Pizza Company</div>
-            <p>
-            </p>
+            <ul>
+              <li>Answering, processing, checking, and completing takeout orders</li>
+              <li>Processing, checking, and completing delivery orders</li>
+              <li>Cleaning before opening or closing the store</li>
+            </ul>
           </div>
           <div className="resume-date text-md-right">
             <span className="text-primary">June 2016 - August 2017</span>
@@ -110,8 +113,29 @@ class Content extends Component {
           <div className="resume-content">
             <h3 className="mb-0">Kennesaw State University</h3>
             <div className="subheading mb-3">Bachelor of Science in Software Engineering</div>
-            <div className="misc-text">Software Engineering</div>
-            <div className="misc-text">Minor in Applied Statistics and Data Analytics</div>
+            <div>Major in Software Engineering</div>
+            <div>Minor in Applied Statistics and Data Analytics</div>
+            <div className="subheading mb-3">Relevent Courses</div>
+            <ul>
+              <li>Programming Principles I&II</li>
+              <li>Introduction to Software Engineering</li>
+              <li>Introduction to Database Systems</li>
+              <li>Discrete Structures in Computing</li>
+              <li>Computer Organization and Architecture</li>
+              <li>Software System Requirements</li>
+              <li>Software Architecture and Design</li>
+              <li>Software Project Management</li>
+              <li>Software Application Domain</li>
+              <li>Software Testing Q&amp;A</li>
+              <li>Data Structures</li>
+              <li>Operating Systems</li>
+              <li>Fundamentals of Block Chain Smart Contracts</li>
+              <li>Computer Applied Statistics</li>
+              <li>Topics in Regression</li>
+              <li>Biostatistics</li>
+              <li>Statistical Methods II</li>
+              <li>Statistics in Python</li>
+            </ul>
           </div>
           <div className="resume-date text-md-right">
             <span className="text-primary">August 2013 - July 2019</span>
@@ -121,6 +145,7 @@ class Content extends Component {
         <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
           <div className="resume-content">
             <h3 className="mb-0">Brookwood High School</h3>
+            <div className="subheading mb-3">College Prepatory Education</div>
           </div>
           <div className="resume-date text-md-right">
             <span className="text-primary">August 2009 - May 2013</span>
@@ -139,57 +164,115 @@ class Content extends Component {
         <div className="subheading mb-3">Programming Languages &amp; Tools</div>
         <ul className="list-inline dev-icons">
           <li className="list-inline-item">
-            <i className="fab fa-html5"></i>
+            <i className="fab fa-html5" data-tip data-for='html'></i>
+            <ReactTooltip place='right' id='html' aria-haspopup='true' role='example'>
+             <p className="rtt">HTML5 </p>
+             <p className="rtt">Skill Level: Advanced</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-css3-alt"></i>
+            <i className="fab fa-css3-alt" data-tip data-for='css'></i>
+            <ReactTooltip place='right' id='css' aria-haspopup='true' role='example'>
+             <p className="rtt">CSS3</p>
+             <p className="rtt">Skill Level: Advanced</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-js-square"></i>
+            <i className="fab fa-js-square" data-tip data-for='js'></i>
+            <ReactTooltip place='right' id='js' aria-haspopup='true' role='example'>
+             <p className="rtt">JavaScript</p>
+             <p className="rtt">Skill Level: Intermediate</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-python"></i>
+            <i className="fab fa-python"data-tip data-for='py'></i>
+            <ReactTooltip place='right' id='py' aria-haspopup='true' role='example'>
+             <p className="rtt">Python</p>
+             <p className="rtt">Skill Level: Intermediate</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-bootstrap"></i>
+            <i className="fab fa-bootstrap"data-tip data-for='bts'></i>
+            <ReactTooltip place='right' id='bts' aria-haspopup='true' role='example'>
+             <p className="rtt">Bootstrap</p>
+             <p className="rtt">Skill Level: Advanced</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-react"></i>
+            <i className="fab fa-react"data-tip data-for='react'></i>
+            <ReactTooltip place='right' id='react' aria-haspopup='true' role='example'>
+             <p className="rtt">ReactJS</p>
+             <p className="rtt">Skill Level: Intermediate</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-node-js"></i>
+            <i className="fab fa-node-js"data-tip data-for='node'></i>
+            <ReactTooltip place='right' id='node' aria-haspopup='true' role='example'>
+             <p className="rtt">Node.JS</p>
+             <p className="rtt">Skill Level: Beginner</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-sass"></i>
+            <i className="fab fa-sass"data-tip data-for='sass'></i>
+            <ReactTooltip place='right' id='sass' aria-haspopup='true' role='example'>
+             <p className="rtt">Sass</p>
+             <p className="rtt">Skill Level: Intermediate</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-linux"></i>
+            <i className="fab fa-linux"data-tip data-for='linux'></i>
+            <ReactTooltip place='right' id='linux' aria-haspopup='true' role='example'>
+             <p className="rtt">Linux</p>
+             <p className="rtt">Skill Level: Advanced</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-gulp"></i>
+            <i className="fab fa-windows"data-tip data-for='windows'></i>
+            <ReactTooltip place='right' id='windows' aria-haspopup='true' role='example'>
+             <p className="rtt">Windows[Office &amp; Command Prompt]</p>
+             <p className="rtt">Skill Level: Advanced</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-angular"></i>
+            <i className="fab fa-gulp"data-tip data-for='gulp'></i>
+            <ReactTooltip place='right' id='gulp' aria-haspopup='true' role='example'>
+             <p className="rtt">GulpJS</p>
+             <p className="rtt">Skill Level: Beginner</p>
+            </ReactTooltip>
           </li>
           <li className="list-inline-item">
-            <i className="fab fa-java"></i>
+            <i className="fab fa-angular"data-tip data-for='ng'></i>
+            <ReactTooltip place='right' id='ng' aria-haspopup='true' role='example'>
+             <p className="rtt">AngularJS</p>
+             <p className="rtt">Skill Level: Beginner</p>
+            </ReactTooltip>
+          </li>
+          <li className="list-inline-item">
+            <i className="fab fa-java"data-tip data-for='java'></i>
+            <ReactTooltip place='right' id='java' aria-haspopup='true' role='example'>
+             <p className="rtt">Java</p>
+             <p className="rtt">Skill Level: Intermediate</p>
+            </ReactTooltip>
+          </li>
+          <li className="list-inline-item">
+            <i className="fas fa-code" data-tip data-for='c'></i>
+            <ReactTooltip place='right' id='c' aria-haspopup='true' role='example'>
+             <p className="rtt">C++</p>
+             <p className="rtt">Skill Level: Intermediate</p>
+            </ReactTooltip>
           </li>
         </ul>
 
-        <div className="subheading mb-3">Workflow</div>
-        <ul className="fa-ul mb-0">
+        <div className="subheading mb-3">Additional Skills</div>
+        <ul>
           <li>
-            <i className="fa-li fa fa-check"></i>
-            Mobile-First, Responsive Design</li>
+            Django Web Framework</li>
           <li>
-            <i className="fa-li fa fa-check"></i>
-            Cross Browser Testing &amp; Debugging</li>
+            Customer Service</li>
           <li>
-            <i className="fa-li fa fa-check"></i>
-            Cross Functional Teams</li>
+            Computer &amp; Phone Repair</li>
           <li>
-            <i className="fa-li fa fa-check"></i>
-            Agile Development &amp; Scrum</li>
+            Agile Development Practices</li>
         </ul>
       </div>
     </section>
@@ -199,9 +282,8 @@ class Content extends Component {
     <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
       <div className="w-100">
         <h2 className="mb-5">Interests</h2>
-        <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-        <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end
-          web development world.</p>
+        <p>When I&apos;m not improving my coding skills, I like to stay active by playing basketball, exercising, or simply being outdoors. I love hiking and kayaking when the weather permits, however, if it’s a nice day outside I will probably be down for whatever.</p>
+        <p className="mb-0">My indoor hobbies aside from staring out the window are playing the guitar or drums, attempting to learn the keyboard, messing around on FL studio, watching SportsCenter, and catching up on innovations in technology.</p>
       </div>
     </section>
 
@@ -213,14 +295,13 @@ class Content extends Component {
         <ul className="fa-ul mb-0">
           <li>
             <i className="fa-li fa fa-trophy text-warning"></i>
-            SoloLearn HTML Fundementals Certificate</li>
+            <a className="certs" href="https://www.sololearn.com/Certificate/1014-1104458/pdf/" target="_blank" rel="noopener noreferrer">SoloLearn HTML Fundementals Certificate</a></li>
           <li>
             <i className="fa-li fa fa-trophy text-warning"></i>
-            SoloLearn CSS Fundementals Certificate</li>
+            <a className="certs" href="https://www.sololearn.com/Certificate/1023-1104458/pdf/" target="_blank" rel="noopener noreferrer">SoloLearn CSS Fundementals Certificate</a></li>
         </ul>
       </div>
     </section>
-    </div>
   </div>
     );
   }
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Assets/css/resume.min.css';
 import 'jquery.easing/jquery.easing.min.js';
 import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
